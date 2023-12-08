6c6142eb883c9ece7d0be13285f7c72b92ad4e4b
addiment <63932310+addiment@users.noreply.github.com>
2023-12-08 17:50:18 -0500

    change orientation

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/BotShared.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/BotShared.kt
@@ -53,8 +53,8 @@ class BotShared(opMode: OpMode) {
 
     init {
         // IMU orientation/calibration
-        val logo = LogoFacingDirection.LEFT
-        val usb = UsbFacingDirection.BACKWARD
+        val logo = LogoFacingDirection.RIGHT
+        val usb = UsbFacingDirection.FORWARD
         val orientationOnRobot = RevHubOrientationOnRobot(logo, usb)
         imu.initialize(IMU.Parameters(orientationOnRobot))
         imu.resetYaw()
