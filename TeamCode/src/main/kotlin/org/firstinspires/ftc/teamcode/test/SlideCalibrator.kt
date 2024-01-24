f582ccee3a1001c6a85a6ca6ad020e453fdcdbce
addiment <63932310+addiment@users.noreply.github.com>
2024-01-24 12:47:33 -0500

    should fix nullability

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/test/SlideCalibrator.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/test/SlideCalibrator.kt
@@ -3,26 +3,36 @@ package org.firstinspires.ftc.teamcode.test
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.BotShared
+import org.firstinspires.ftc.teamcode.botmodule.BotModule
+import org.firstinspires.ftc.teamcode.botmodule.BotModule.StatusEnum.OK
 import org.firstinspires.ftc.teamcode.botmodule.LSD
 import org.firstinspires.ftc.teamcode.botmodule.ModuleConfig
 import kotlin.math.roundToInt
 
 @TeleOp
 class SlideCalibrator : OpMode() {
-    val shared = BotShared(this)
+    private lateinit var shared: BotShared
     private lateinit var lsd: LSD
+
+
+    override fun init() {
+        shared = BotShared(this)
+        lsd = LSD(ModuleConfig(this, shared, true, null))
+    }
 
     override fun start() {
         lsd.modStart()
     }
-    override fun init() {
-        lsd = LSD(ModuleConfig(this, shared, true, null))
-    }
 
     override fun loop() {
-        lsd.targetHeight += (-gamepad1.left_stick_y * 200f).roundToInt()
-        lsd.modUpdate()
-        telemetry.addData("Slide Height", lsd.targetHeight)
+        if (lsd.status.status == OK) {
+            lsd.targetHeight += (-gamepad1.left_stick_y * 200f).roundToInt()
+            lsd.modUpdate()
+            telemetry.addData("Slide Height", lsd.targetHeight)
+        } else {
+            telemetry.addLine("lsd is not ok :(");
+            telemetry.addLine(lsd.status.hardwareMissing?.joinToString());
+        }
         telemetry.update()
     }
 
