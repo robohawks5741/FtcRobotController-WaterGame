535ad075819e00b48f4b3ab6452335adc48a7754
addiment <63932310+addiment@users.noreply.github.com>
2024-01-24 13:16:06 -0500

    the power was 0 >:(

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/test/SlideCalibrator.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/test/SlideCalibrator.kt
@@ -2,38 +2,70 @@ package org.firstinspires.ftc.teamcode.test
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import org.firstinspires.ftc.teamcode.BotShared
-import org.firstinspires.ftc.teamcode.botmodule.BotModule
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.DcMotorSimple
 import org.firstinspires.ftc.teamcode.botmodule.BotModule.StatusEnum.OK
-import org.firstinspires.ftc.teamcode.botmodule.LSD
-import org.firstinspires.ftc.teamcode.botmodule.ModuleConfig
 import kotlin.math.roundToInt
 
 @TeleOp
 class SlideCalibrator : OpMode() {
-    private lateinit var shared: BotShared
-    private lateinit var lsd: LSD
-
+    private lateinit var slideLeft: DcMotorEx
+    private lateinit var slideRight: DcMotorEx
 
     override fun init() {
-        shared = BotShared(this)
-        lsd = LSD(ModuleConfig(this, shared, true, null))
+
+        slideLeft = hardwareMap[DcMotorEx::class.java, "slideL"]
+        slideRight = hardwareMap[DcMotorEx::class.java, "slideR"]
+
+        slideLeft.targetPosition = 0
+        slideRight.targetPosition = 0
+        slideLeft.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        slideRight.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        slideLeft.mode = DcMotor.RunMode.RUN_TO_POSITION
+        slideRight.mode = DcMotor.RunMode.RUN_TO_POSITION
+
+        slideLeft.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
+        slideRight.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
+
+        // Directions
+        slideLeft.direction = DcMotorSimple.Direction.FORWARD
+        slideRight.direction = DcMotorSimple.Direction.REVERSE
     }
 
-    override fun start() {
-        lsd.modStart()
-    }
+    private var targetHeightLeft: Float = 0f
+    private var targetHeightRight: Float = 0f
 
     override fun loop() {
-        if (lsd.status.status == OK) {
-            lsd.targetHeight += (-gamepad1.left_stick_y * 200f).roundToInt()
-            lsd.modUpdate()
-            telemetry.addData("Slide Height", lsd.targetHeight)
+        targetHeightLeft = (targetHeightLeft - gamepad1.left_stick_y).coerceAtLeast(0f)
+        targetHeightRight = (targetHeightRight - gamepad1.right_stick_y).coerceAtLeast(0f)
+
+        if (gamepad1.a) {
+            slideLeft.power = 1.0
+            slideRight.power = 1.0
+
+            slideLeft.targetPosition = targetHeightLeft.roundToInt().coerceAtLeast(0)
+            slideRight.targetPosition = targetHeightRight.roundToInt().coerceAtLeast(0)
+
+            slideLeft.mode = DcMotor.RunMode.RUN_TO_POSITION
+            slideRight.mode = DcMotor.RunMode.RUN_TO_POSITION
+
+            slideLeft.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+            slideRight.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
         } else {
-            telemetry.addLine("lsd is not ok :(");
-            telemetry.addLine(lsd.status.hardwareMissing?.joinToString());
+            slideLeft.power = 0.0
+            slideRight.power = 0.0
+
+            slideLeft.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
+            slideRight.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.FLOAT
         }
+        telemetry.addData("Target Height L (var)", targetHeightLeft)
+        telemetry.addData("Target Height R (var)", targetHeightRight)
+        telemetry.addData("Target Height L", slideLeft.targetPosition)
+        telemetry.addData("Target Height R", slideRight.targetPosition)
+        telemetry.addData("Actual Height L", slideLeft.currentPosition)
+        telemetry.addData("Actual Height R", slideRight.currentPosition)
+
         telemetry.update()
     }
-
 }
